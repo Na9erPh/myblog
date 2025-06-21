214c16ecc1df43e46798a7c7bd112a78747fb29a
Na9erPH <t9100008@rb.moe.gov.sa>
2025-06-21 06:24:27 +0300

    إضافة Netlify Identity Widget لحل مشكلة روابط الدعوة

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -11,6 +11,7 @@ export default function Layout({ children }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Amiri:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.css" integrity="sha384-GvrOXuhMATgEsSwCs4smul74iXGOixntILdUW9XmUC6+HX0sLNAK3q71HotJqlAn" crossOrigin="anonymous" />
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>
 
       <header className="bg-primary text-white p-4 text-center">
@@ -22,6 +23,20 @@ export default function Layout({ children }) {
       <footer className="bg-gray-100 text-center p-4 mt-16">
         © {new Date().getFullYear()} جميع الحقوق محفوظة
       </footer>
+      
+      <script dangerouslySetInnerHTML={{
+        __html: `
+          if (window.netlifyIdentity) {
+            window.netlifyIdentity.on("init", user => {
+              if (!user) {
+                window.netlifyIdentity.on("login", () => {
+                  document.location.href = "/admin/";
+                });
+              }
+            });
+          }
+        `
+      }} />
     </>
   );
 } 
